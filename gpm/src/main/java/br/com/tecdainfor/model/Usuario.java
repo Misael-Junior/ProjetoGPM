ff2752857c071e30c9b4f2f14aee3f8707c6dd63
Misael_Junior <junioribeiro-2009@hotmail.com>
2017-09-18 22:36:34 -0300

    Atualização da Classe Usuário.

--- gpm/src/main/java/br/com/tecdainfor/model/Usuario.java
+++ gpm/src/main/java/br/com/tecdainfor/model/Usuario.java
@@ -1,35 +1,46 @@
 package br.com.tecdainfor.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.Size;
 
 import org.hibernate.validator.constraints.NotEmpty;
 
+// Classe Usuário.
+
 @Entity
+@Table(name = "TB_USUARIO")
 public class Usuario {
 	
 	//Atributos da classe usuário
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_usuario_pk", nullable = false)
 	private int codigo;
 	@NotEmpty(message = "Nome é obrigatória")
 	@Size(max = 60, message = "O Nome não pode conter mais de 60 caracteres")
+	@Column(name = "st_nome", nullable = false)
 	private String nome;
 	@NotEmpty(message = "Email é obrigatória")
 	@Size(max = 60, message = "O Email não pode conter mais de 60 caracteres")
+	@Column(name = "st_email", nullable = false)
 	private String email;
 	@NotEmpty(message = "Senha é obrigatória")
 	@Size(max = 60, message = "A Senha não pode conter mais de 60 caracteres")
+	@Column(name = "st_senha", nullable = false)
 	private String senha;
 	@NotEmpty(message = "Telefone é obrigatória")
 	@Size(max = 60, message = "O Telefone não pode conter mais de 60 caracteres")
+	@Column(name = "st_telefone", nullable = false)
 	private String telefone;
 	@NotEmpty(message = "Perfil é obrigatória")
 	@Size(max = 60, message = "O Perfil não pode conter mais de 60 caracteres")
+	@Column(name = "st_perfil", nullable = false)
 	private String perfil;
 	
 	//Gets e Sets da classe usuário.
@@ -69,6 +80,5 @@ public class Usuario {
 	}
 	public void setPerfil(String perfil) {
 		this.perfil = perfil;
-	}
-		
+	}	
 }
